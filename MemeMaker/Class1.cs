0708ef42bb0eb7b134b881130c5e06f35a692efb
Marcos Alvira Romero <marcos.alvira@iesdoctorbalmis.com>
2019-10-23 14:11:37 +0200

    Agregar archivos de proyecto.

---
+++ MemeMaker/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class BorderConverte : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool check = (bool)value;
+
+            if (check==true)
+            {
+                return 5;
+            }
+            else
+            {
+                return 0;
+            }
+
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
